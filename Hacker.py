673e43aa50620bcb2bf43c2b743f0f5a9258d90c
jamie <himjy003@mymail.unisa.edu.au>
2025-10-09 15:47:52 +1030

    Create Hacker class with trace_level method

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,21 @@
 """
 File: Hacker.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
+Author: Jamie Him
+ID: 110375225
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    def __init__(self):
+        self.name = 'Hacker'
+        self.CryptoToken = 1
+        self.rig = 0
+        self.trace_level = 0
+
+    def trace_level(self):
+        trace_level = self.trace_level + 1
+        if trace_level >= 5:
+            print(f"Hacker is exposed")
+
