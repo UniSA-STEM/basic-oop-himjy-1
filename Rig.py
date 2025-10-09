e7b5c2d825b05fcffa0a1492f229865f24091be0
jamie <himjy003@mymail.unisa.edu.au>
2025-10-09 16:18:07 +1030

    Created rig class and methods

--- Rig.py
+++ Rig.py
@@ -11,3 +11,29 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 class Rig:
     def __init__(self):
         self.name = "Rig"
+        self.damage = 0
+        self.broken_state = False
+        self.storage = 0
+        self.data_spike = 2
+        self.removeable_drive = 1
+        self.upgrade_level = 0
+
+    def repaired(self):
+        pass
+
+    def upgraded(self):
+        pass
+
+    def data_spike(self):
+        pass
+
+    def generate_asset(self):
+        pass
+
+    def store(self):
+        pass
+
+
+
+
+
